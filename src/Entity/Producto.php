e181b9a47bc8fa35d7f626006c38af5c57bac871
Juan Ardissone <micayael@gmail.com>
2019-07-18 18:00:08 -0400

    Se agregaron las tablas

---
+++ src/Entity/Producto.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductoRepository")
+ */
+class Producto
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $precio;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imagen;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $nuevo;
+
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $precioAnterior;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descripcion;
+
+    public function __construct()
+    {
+        $this->categorias = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): self
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    public function getNuevo(): ?bool
+    {
+        return $this->nuevo;
+    }
+
+    public function setNuevo(bool $nuevo): self
+    {
+        $this->nuevo = $nuevo;
+
+        return $this;
+    }
+
+    public function getPrecioAnterior(): ?float
+    {
+        return $this->precioAnterior;
+    }
+
+    public function setPrecioAnterior(?float $precioAnterior): self
+    {
+        $this->precioAnterior = $precioAnterior;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+}
